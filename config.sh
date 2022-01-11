4af11ede074d53b933133533b6e0020430038168
d-pettersson <david.pettersson@code.berlin>
2022-01-11 17:14:51 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# RPC config
 TEZOS_VERSION="v12.0-rc1"
 TEZOS_PATH="/tezos"
+RPC_URL="testnode.somaticbits.xyz"
+
+# Grafana + Prometheus config
+MONITOR_URL="testmonitor.somaticbits.xyz"
+
+# TzKT config
+TZKT_URL="testtzkt.somaticbits.xyz"
