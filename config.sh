8e852000c0c52939b30661ecfbcd65206e27bb24
d-pettersson <david.pettersson@code.berlin>
2022-01-11 17:40:36 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,9 +4,12 @@
 TEZOS_VERSION="v12.0-rc1"
 TEZOS_PATH="/tezos"
 RPC_URL="testnode.somaticbits.xyz"
+RPC_PORT=8732 # default port
 
 # Grafana + Prometheus config
 MONITOR_URL="testmonitor.somaticbits.xyz"
+MONITOR_PORT=3000
 
 # TzKT config
 TZKT_URL="testtzkt.somaticbits.xyz"
+TZKT_PORT=5000
