dae1d726b7e3a1202168122387af289c62a219c6
d-pettersson <david.pettersson@code.berlin>
2022-01-11 11:53:05 +0100

    Edit post-install

--- CentOS-post-install.sh
+++ CentOS-post-install.sh
@@ -6,50 +6,52 @@ blue=$'\e[34m'
 endColor=$'\e[0m'
 
 echo
+echo -e "${yellow}******************************************************${endColor}"
 echo -e "${yellow}Welcome to SOMATICBITS's Centos 7 post install script!${endColor}"
+echo -e "${yellow}******************************************************${endColor}"
 echo
 echo -e "----------- Updating system..."
 echo
 
-yum update -yyy
+yum update -yyy -q
 
 echo
-echo -e "${green}System is updated.${endColor}"
+echo -e "${green}✓ System is updated.${endColor}"
 echo
-echo "----------- Adding Epel repo..."
-echo
-
-yum install epel-release -y
-
-echo
-echo -e "${green}✓ Done!${endColor}"
+echo "----------- ${yellow}Adding Epel repo...${endColor}"
 echo
 
-yum update
-
-echo
-echo -e "----------- Installing Development tools and few other things..."
-echo
-
-yum install nano git -y
-yum group install 'Development Tools' -y
-yum install yum-utils device-mapper-persistent-data lvm2 -y
-yum install python-pip -y
-pip install --upgrade pip
+yum install epel-release -y -q
 
 echo
 echo -e "${green}✓ Done!${endColor}"
 echo
 
-echo -e "----------- Adding Docker Community Edition repository and installing docker-ce with docker-compose..."
+yum update -q
+
+echo
+echo -e "----------- ${yellow}Installing Development tools and few other things...${endColor}"
+echo
+
+yum install nano git -y -q
+yum group install 'Development Tools' -y -q
+yum install yum-utils device-mapper-persistent-data lvm2 -y -q
+yum install python-pip -y -q
+pip install --upgrade pip -q
+
+echo
+echo -e "${green}✓ Done!${endColor}"
+echo
+
+echo -e "----------- ${yellow}Adding Docker Community Edition repository and installing docker-ce with docker-compose...${endColor}"
 platform=$(uname -s)
 curl -L 'https://github.com/docker/compose/releases/latest/download/docker-compose-${platform,}-$(uname -m)'  -o /usr/local/bin/docker-compose
 cp /usr/local/bin/docker-compose /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
-yum upgrade python*
+yum upgrade python* -q
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum update
-yum install docker-ce -yy
+yum update -q
+yum install docker-ce -yy -q
 systemctl start docker
 systemctl enable docker
 
@@ -57,8 +59,8 @@ echo
 echo -e "${green}✓ Done!${endColor}"
 echo
 
-echo "----------- Installing NTP syncing..."
-yum install ntp ntpdate -y
+echo "----------- ${yellow}Installing NTP syncing...${endColor}"
+yum install ntp ntpdate -y -q
 systemctl start ntpd
 systemctl enable ntpd
 ntpdate -u -s 0.de.pool.ntp.org 1.de.pool.ntp.org 2.de.pool.ntp.org 3.de.pool.ntp.org
