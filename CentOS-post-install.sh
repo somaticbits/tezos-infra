fee6e7843b7de36f4cf3dfe371503a09709c6cdb
d-pettersson <david.pettersson@code.berlin>
2022-01-11 11:32:32 +0100

    Edit post-install

--- CentOS-post-install.sh
+++ CentOS-post-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 bold=$'\e[1;0m'
+italic=$'\e[3:0m'
 red=$'\e[0;31m'
 boldRed=$'\e[1;31m'
 green=$'\e[0;32m'
@@ -8,28 +9,28 @@ yellow=$'\e[0;33m'
 boldYellow=$'\e[1;33m'
 blue=$'\e[0;34m'
 boldBlue=$'\e[1;34m'
-close=$'\e[0m'
+endColor=$'\e[0m'
 
-echo '\e[1;0m'Welcome to SOMATICBITS\'s Centos 7 post install script!'\e[0m'
+echo -e "${bold}Welcome to SOMATICBITS's Centos 7 post install script!${endColor}"
 echo
-echo 'Updating system...'
+echo -e "${italic}Updating system...${endColor}"
 echo
 yum update -yyy
 
-echo '\e[1;32m'System is updated.'\e[0m'
+echo -e "${boldGreen}System is updated.${endColor}"
 echo
-echo 'Adding Epel repo...'
+echo "${italic}Adding Epel repo...${endColor}"
 
 yum install epel-release -y
 
 echo
-echo '\e[1;32m'Done!'\e[0m'
+echo -e "${boldGreen}Done!${endColor}"
 echo
 
 yum update
 
 echo
-echo '\e[1;0m'Installing Development tools and few other things...'\e[0m'
+echo -e "${bold}Installing Development tools and few other things...${endColor}"
 echo
 
 yum install nano git -y
@@ -39,10 +40,10 @@ yum install python-pip -y
 pip install --upgrade pip
 
 echo
-echo '\e[1;32m'Done!'\e[0m'
+echo -e "${boldGreen}Done!${endColor}"
 echo
 
-echo '\e[1;0m'Adding Docker Community Edition repository and installing docker-ce with docker-compose...'\e[0m'
+echo -e "${italic}Adding Docker Community Edition repository and installing docker-ce with docker-compose...${endColor}"
 platform=$(uname -s)
 curl -L 'https://github.com/docker/compose/releases/latest/download/docker-compose-${platform,}-$(uname -m)'  -o /usr/local/bin/docker-compose
 cp /usr/local/bin/docker-compose /usr/bin/docker-compose
@@ -55,10 +56,10 @@ systemctl start docker
 systemctl enable docker
 
 echo
-echo '\e[1;32m'Done!'\e[0m'
+echo -e "${boldGreen}Done!${endColor}"
 echo
 
-echo '\e[1;0m'Installing NTP syncing...'\e[0m'
+echo "${italic}Installing NTP syncing...${endColor}"
 yum install ntp ntpdate
 systemctl start ntpd
 systemctl enable ntpd
@@ -67,7 +68,7 @@ systemctl restart ntpd
 hwclock -w
 
 echo
-echo '\e[1;32m'Done!'\e[0m'
+echo -e "${boldGreen}Done!${endColor}"
 echo
 
 mkdir .ssh
