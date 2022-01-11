f4bfa3edbaf06bfd278b7c1db108e161ff8de46a
d-pettersson <david.pettersson@code.berlin>
2022-01-11 17:11:06 +0100

    Add install-nginx.sh script

---
+++ install-nginx.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+red=$'\e[31m'
+green=$'\e[32m'
+yellow=$'\e[33m'
+blue=$'\e[34m'
+endColor=$'\e[0m'
+
+source ./config.sh
+
+function createAvailableSite() {
+  if true; then
+    cat <<- EOF > $@
+    server {}
+    EOF
+}
+
+echo
+echo -e "${yellow}**********************************************************${endColor}"
+echo -e "${yellow}* Welcome to SOMATICBITS's NGINX and SSL install script! *${endColor}"
+echo -e "${yellow}**********************************************************${endColor}"
+echo
+echo -e "${yellow}--- Installing NGINX${endColor}"
+echo
+
+#yum install nginx -q
+
+echo
+echo -e "${green}✓ Done!${endColor}"
+echo
+
+#mkdir /etc/nginx/sites-available
+#mkdir /etc/nginx/sites-enabled
+
+#mv ./nginx.conf /etc/nginx/nginx.conf
+
+createAvailableSite ${RPC_URL}
