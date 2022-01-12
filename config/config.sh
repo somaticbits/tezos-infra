463bb1dbb1098e6d7e2d7eb086ef85cfee2091e2
d-pettersson <david.pettersson@code.berlin>
2022-01-12 17:07:56 +0100

    Change URLs

--- config/config.sh
+++ config/config.sh
@@ -4,7 +4,7 @@
 TEZOS_VERSION="v12.0-rc1_7a8c3312_20211220191531"
 TEZOS_HISTORY_MODE="${TEZOS_HISTORY_MODE:-full}"
 TEZOS_PATH="/tezos"
-RPC_URL="testnode.somaticbits.xyz"
+RPC_URL="t-node.somaticbits.xyz"
 RPC_PORT=8732 # default port
 
 # Grafana + Prometheus config
@@ -12,7 +12,7 @@ MONITOR_URL="testmonitor.somaticbits.xyz"
 MONITOR_PORT=3000 # default port
 
 # TzKT config
-TZKT_URL="testtzkt.somaticbits.xyz"
+TZKT_URL="t-tzkt.somaticbits.xyz"
 TZKT_PORT=5000 # default port
 
 # Certbot config
