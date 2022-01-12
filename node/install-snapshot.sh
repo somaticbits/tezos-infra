8735747ef575f6ac1a8aaa92f301263f8861c49b
d-pettersson <david.pettersson@code.berlin>
2022-01-12 10:06:27 +0100

    Fix SNAPSHOT_URL

--- node/install-snapshot.sh
+++ node/install-snapshot.sh
@@ -10,6 +10,8 @@ source ./config.sh
 echo -e "${yellow}--- Importing snapshot (will take a while... be patient)${endColor}"
 echo
 
+SNAPSHOT_URL=$(curl -sS https://snapshots-tezos.giganode.io | grep -o -E '"(https.*mainnet.*full)"' | tr -d '"' | head -1)
+
 # Extract block id from snapshot URL for import
 BLOCK_ID=$(echo "$SNAPSHOT_URL" | grep -o -P '(?<=[0-9]_)(.*?)(?=.full)')
 docker run --rm -v ${TEZOS_PATH}/node:/var/run/tezos/node \
